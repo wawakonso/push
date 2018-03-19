9394fda21534e9051e7236164ee5877c2ba954c1
Konso Wawa <wawakonso@gmail.com>
2018-03-19 12:57:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,11 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-    //const _data = JSON.parse(event.data.text());
+    const _data = JSON.parse(event.data.text());
       
-    const title = 'Push Instal';
+    const title = _data.title || 'Push Instal';
     const options = {
-      body: `${event.data.text()}`,
+      body: `${_data.message}`,
       icon: 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
       badge: 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
       image: 'https://scontent-mxp1-1.xx.fbcdn.net/v/t1.0-9/28795572_1812946842078123_1160688682378723328_o.jpg?oh=d4f6b2907ec9d8e8aa0dacceb239436a&oe=5B2D0F7F',
