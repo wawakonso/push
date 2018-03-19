cdd9b814ff069741230f9cac31117529813a2d9d
Konso Wawa <wawakonso@gmail.com>
2018-03-19 11:27:00 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,15 +2,14 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+    const _data = JSON.parse(event.data.text());
   
-    const title = 'Push Instal';
+    const title = _data.title || 'Push Instal';
     const options = {
-      body: `${event.data.text()}`,
-      icon: 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
-      badge: 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
-      image: 'https://scontent-mxp1-1.xx.fbcdn.net/v/t1.0-9/28795572_1812946842078123_1160688682378723328_o.jpg?oh=d4f6b2907ec9d8e8aa0dacceb239436a&oe=5B2D0F7F',
+      body: `${_data.message}`,
+      icon: `${_data.icon}` || 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
+      badge: `${_data.badge}` || 'https://images.contentful.com/u5xczhazxdam/6gu1fH9IJiCUI8Aq8Mw2UY/f6a505a2ea6522c920250e1cd96cf501/social-share.jpg',
+      image: `${_data.image }` || 'https://scontent-mxp1-1.xx.fbcdn.net/v/t1.0-9/28795572_1812946842078123_1160688682378723328_o.jpg?oh=d4f6b2907ec9d8e8aa0dacceb239436a&oe=5B2D0F7F',
       actions: [
         {
           action: 'panel-close',
