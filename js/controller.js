7fe13673c3c7ca045bd63f65d4d7497c63d968b3
Konso Wawa <wawakonso@gmail.com>
2018-03-21 12:52:23 +0100

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,7 @@ let swRegistration = null;
 
 var uniqueId = function create_UUID(){
     var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var uuid = 'xxxxxx3xxxxxx4xxxyxxx0xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = (dt + Math.random()*16)%16 | 0;
         dt = Math.floor(dt/16);
         return (c=='x' ? r :(r&0x3|0x8)).toString(16);
